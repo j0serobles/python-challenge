b98e2c13a95b4cf8726937a410b8499231280572
Jose Robles <engjoserobles@gmail.com>
2021-01-09 23:19:06 -0500

    Added main.py files with algorithm for the solution

---
+++ PyBank/main.py
@@ -0,0 +1,31 @@
+"""
+===============================================================
+.NAME
+    PyBank/main.py
+.WHAT
+    PyBank main program, UCF Data Analysis and Visualization
+    Bootcamp homework #3
+.DATE
+    09-JAN-2020
+.AUTHOR
+    engjoserobles@gmail.com
+===============================================================
+"""
+
+# Open csv file from Resources directory
+
+#Traverse each line in the file, for each line
+#    Define a variable for the number of months and increment it for each line
+#    Define an accumulator for the net amount of profit loss
+#    Define a structure to store the change in Profit/Loss for each month, compared to the previous one
+#    Save the greatest increase in profit in a variable
+#    Save the greatest decrease in losses in a variable
+
+# Print the results to the terminal and to a CSV file as:
+# Financial Analysis
+# ----------------------------
+# Total Months: nn
+# Total: $nnnnnnnnnn
+# Average  Change: $nnnnnn.nn
+# Greatest Increase in Profits: Mon-YYYY ($nnnnnnnn)
+# Greatest Decrease in Profits: Mon-YYYY ($nnnnnnnn)
---
+++ PyPoll/main.py
@@ -0,0 +1,37 @@
+"""
+===============================================================
+.NAME
+    PyPool/main.py
+.WHAT
+    PyPool main program, UCF Data Analysis and Visualization
+    Bootcamp homework #3
+.DATE
+    09-JAN-2020
+.AUTHOR
+    engjoserobles@gmail.com
+===============================================================
+"""
+
+# Open csv file from Resources directory
+
+#Traverse each line in the file, for each line
+#    Define a variable for the number of votes cast
+#    Define a structure to store the candidates that received votes
+#    Define a structure to store the total number of votes each candidate won
+#    Define a structure for the percentage of the total votes each candidate won
+#    Define a variable to store the winner of the election.
+
+
+
+# Print the results to the terminal and to a CSV file as:
+# Election Results
+# -------------------------
+# Total Votes: nnnnnnnnnnn
+# -------------------------
+# Khan: NN.000% (NNNNNNNN)
+# Correy: NN.000% (NNNNNN)
+# Li: NN.000% (NNNNNNN)
+# O'Tooley: NN.000% (NNNN)
+# -------------------------
+# Winner: <Name> 
+# -------------------------
